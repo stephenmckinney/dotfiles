9fbac772c7fdfa4b79d4ec991141382211d2cd23
Steve McKinney <steve.mckinney@gmail.com>
2019-10-29 19:52:46 -0400

    Add ram to right prompt
    Good to know with the duo of Docker and Slack eating all my memory

--- p10k.zsh
+++ p10k.zsh
@@ -47,23 +47,23 @@ fi
       status                  # exit code of the last command
       command_execution_time  # duration of the last command
       background_jobs         # presence of background jobs
-      # virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
-      # anaconda                # conda environment (https://conda.io/)
-      # pyenv                   # python environment (https://github.com/pyenv/pyenv)
-      # nodenv                  # node.js version from nodenv (https://github.com/nodenv/nodenv)
-      # nvm                     # node.js version from nvm (https://github.com/nvm-sh/nvm)
-      # nodeenv                 # node.js environment (https://github.com/ekalinin/nodeenv)
+      # virtualenv            # python virtual environment (https://docs.python.org/3/library/venv.html)
+      # anaconda              # conda environment (https://conda.io/)
+      # pyenv                 # python environment (https://github.com/pyenv/pyenv)
+      # nodenv                # node.js version from nodenv (https://github.com/nodenv/nodenv)
+      # nvm                   # node.js version from nvm (https://github.com/nvm-sh/nvm)
+      # nodeenv               # node.js environment (https://github.com/ekalinin/nodeenv)
       # node_version          # node.js version
       # go_version            # go version (https://golang.org)
       # rust_version          # rustc version (https://www.rust-lang.org)
       rbenv                   # ruby version from rbenv (https://github.com/rbenv/rbenv)
-      # rvm                     # ruby version from rvm (https://rvm.io)
-      # kubecontext             # current kubernetes context (https://kubernetes.io/)
-      # terraform               # terraform workspace (https://www.terraform.io)
+      # rvm                   # ruby version from rvm (https://rvm.io)
+      # kubecontext           # current kubernetes context (https://kubernetes.io/)
+      # terraform             # terraform workspace (https://www.terraform.io)
       context                 # user@host
-      # nordvpn                 # nordvpn connection status, linux only (https://nordvpn.com/)
-      # ranger                  # ranger shell (https://github.com/ranger/ranger)
-      # ram                   # free RAM
+      # nordvpn               # nordvpn connection status, linux only (https://nordvpn.com/)
+      # ranger                # ranger shell (https://github.com/ranger/ranger)
+      ram                     # free RAM
       # load                  # CPU load
       time                    # current time
       # public_ip             # public IP address
