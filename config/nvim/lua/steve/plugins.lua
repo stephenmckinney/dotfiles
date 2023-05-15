90182b94eec5ec2f5b66bf102efc7aa1bac736b1
Steve McKinney <steve.mckinney@gmail.com>
2023-05-15 13:15:38 -0400

    [nvim] Add basic fuzzy finder

--- config/nvim/lua/steve/plugins.lua
+++ config/nvim/lua/steve/plugins.lua
@@ -1,3 +1,13 @@
 return {
---  "folke/neodev.nvim",
+  -- fuzzy finder
+  {
+    'nvim-telescope/telescope.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    cmd = 'Telescop',
+    version = false,
+    keys = {
+      { '<leader>t', '<cmd>Telescope find_files<cr>', desc = 'Find Files (root dir)' },
+      { '<leader>b', '<cmd>Telescope buffers<cr>', desc = 'Buffers' }
+    }
+  }
 }
