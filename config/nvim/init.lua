f234a58a88e4b0e138ddb91c620dde6379d0123b
Steve McKinney <steve.mckinney@gmail.com>
2023-05-15 18:40:25 -0400

    [nvim] Add file explorer plugin

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,6 +1,10 @@
 -- Set mapleader before plugins are required, to ensure plugins that rely on mapleader use the correct key
 vim.g.mapleader = ','
 
+-- Disabling the built-in netrw and netrwPlugin in favor of a file explorer plugin.
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
--- config/nvim/lua/steve/plugins.lua
+++ config/nvim/lua/steve/plugins.lua
@@ -3,11 +3,24 @@ return {
   {
     'nvim-telescope/telescope.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
-    cmd = 'Telescop',
+    cmd = 'Telescope',
     version = false,
     keys = {
       { '<leader>t', '<cmd>Telescope find_files<cr>', desc = 'Find Files (root dir)' },
       { '<leader>b', '<cmd>Telescope buffers<cr>', desc = 'Buffers' }
+    }
+  },
+
+  -- file explorer
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("nvim-tree").setup {}
+    end,
+    keys = {
+      { '<leader>d', '<cmd>NvimTreeToggle<cr>', desc = 'Toggle File Explorer' },
     }
   }
 }
