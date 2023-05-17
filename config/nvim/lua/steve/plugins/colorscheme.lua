3f95ab429f040a09ace8587912aead7a3b093f4e
Steve McKinney <steve.mckinney@gmail.com>
2023-05-17 16:18:28 -0400

    [nvim] Add more colorschemes
    Only colorschemes in lua, with treesitter support and itermcolors

--- config/nvim/lua/steve/plugins/colorscheme.lua
+++ config/nvim/lua/steve/plugins/colorscheme.lua
@@ -1,36 +1,63 @@
+--------------------------------------------------------------------------------
+-- Colorschemes
+--
+-- All themes must:
+-- * be written in Lua
+-- * support treesitter
+-- * support lualine
+-- * include itermcolors
+--------------------------------------------------------------------------------
 return {
   -- catppuccin
   {
     'catppuccin/nvim',
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    name = 'catppuccin',
+    lazy = true, -- set to false to load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      require('catppuccin').setup {
-        flavor='macchiato',
-      }
-      vim.cmd([[colorscheme catppuccin]]) -- load colorscheme
-    end
-  },
+      require('catppuccin').setup {}
+      vim.cmd('colorscheme catppuccin-macchiato') -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
 
-  -- solarized
-  {
-    'Tsuzat/NeoSolarized.nvim',
-    lazy = true,
-    config = function()
-      require('NeoSolarized').setup {
-        style = 'dark',
-        transparent = false,
-        enable_italics = false,
-        on_highlights = function(highlights, colors) 
-          highlights.Visual.bg = colors.fg2
-        end,
-      }
-    end
+    end,
   },
 
   -- tokyo night
   {
     'folke/tokyonight.nvim',
-    lazy = true,
+    name = 'tokyonight',
+    lazy = true, -- set to false to load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('tokyonight').setup {
+        style = 'storm' -- storm, moon, night, day
+      }
+      vim.cmd('colorscheme tokyonight')
+    end,
+  },
+
+  -- nightfox
+  { 
+    'EdenEast/nightfox.nvim',
+    name = 'nightfox',
+    lazy = false, -- set to false to load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('nightfox').setup {}
+      vim.cmd('colorscheme terafox') -- nightfox, dayfox, dawnfox, duskfox, nordfox, terafox, carbonfox
+    end,
+  },
+
+  -- rose-pine
+  { 
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    lazy = true, -- set to false to load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('rose-pine').setup {
+        variant = 'moon', --- auto, main, moon, dawn
+      }
+      vim.cmd('colorscheme rose-pine')
+    end,
   },
 }
