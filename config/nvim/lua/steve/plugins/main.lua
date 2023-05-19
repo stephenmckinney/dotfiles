adbd6546c6d786aa90ae06b388c5ab837f7039c3
Steve McKinney <steve.mckinney@gmail.com>
2023-05-18 22:32:13 -0400

    [nvim] Improve incremental selection keymaps

--- config/nvim/lua/steve/plugins/main.lua
+++ config/nvim/lua/steve/plugins/main.lua
@@ -16,8 +16,8 @@ return {
         incremental_selection = {
           enable = true,
           keymaps = {
-            init_selection = '<C-space>',
-            node_incremental = '<C-space>',
+            init_selection = '<leader>i',
+            node_incremental = '<leader>i',
             scope_incremental = false,
             node_decremental = '<bs>',
           },
