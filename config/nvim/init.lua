f28b58424925b674ee73e5e103aa406a16a2dee9
Steve McKinney <steve.mckinney@gmail.com>
2023-05-23 09:55:11 -0400

    [nvim] Update init style and comments
    * Fix lua style
    * Update comments using ChatGPT

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,25 +1,35 @@
--- Set mapleader before plugins are required, to ensure plugins that rely on mapleader use the correct key
-vim.g.mapleader = ','
+-- Define the mapleader variable early in the configuration process. This is important
+-- because some plugins might rely on it.
+vim.g.mapleader = ","
 
--- Disabling the built-in netrw and netrwPlugin in favor of a file explorer plugin.
+-- Disable the built-in netrw and netrwPlugin in order to use a preferred file explorer plugin.
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+-- Bootstrap lazy.nvim, for managing and loading plugins lazily.
+-- The setup checks for the existence of lazy.nvim. If it's not found, it clones the
+-- latest stable version of lazy.nvim from GitHub.
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
     lazypath,
-  }
+  })
 end
 vim.opt.rtp:prepend(lazypath)
-require('lazy').setup('steve.plugins')
+require("lazy").setup("steve.plugins", {
+  -- disable change notification
+  change_detection = {
+    notify = false,
+  },
+})
 
--- Set configs
-require 'steve.options'
-require 'steve.keymaps'
+-- Load the custom configuration modules for NeoVim options and keymaps.
+-- 'steve.options' contains settings for various Vim options.
+-- 'steve.keymaps' contains custom key mappings.
+require("steve.options")
+require("steve.keymaps")
