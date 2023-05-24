3e96c1ebce145e38cc5b2d859bdcfaace17d1487
Steve McKinney <steve.mckinney@gmail.com>
2023-05-23 20:56:27 -0400

    [nvim] Add comments to statuscol plugin

--- config/nvim/lua/steve/plugins/ui.lua
+++ config/nvim/lua/steve/plugins/ui.lua
@@ -25,16 +25,14 @@ return {
       local builtin = require("statuscol.builtin")
       require("statuscol").setup({
         segments = {
+          -- fold column
           { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
-          {
-            sign = { name = { "Diagnostic" }, maxwidth = 2, auto = true },
-            click = "v:lua.ScSa",
-          },
+          -- diagnostic signs
+          { sign = { name = { "Diagnostic" }, maxwidth = 2, auto = true }, click = "v:lua.ScSa" },
+          -- numbers
           { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
-          {
-            sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
-            click = "v:lua.ScSa",
-          },
+          -- a sign segment that is only 1 cell wide that shows all other signs
+          { sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true }, click = "v:lua.ScSa" },
         },
       })
     end,
