c2ffb436d8c3956421a5523fc555bf964e7c884a
Steve McKinney <steve.mckinney@gmail.com>
2023-05-26 18:31:54 -0400

    [nvim] Use 'q' in fugitive window

--- config/nvim/lua/steve/autocmds.lua
+++ config/nvim/lua/steve/autocmds.lua
@@ -61,6 +61,7 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 vim.api.nvim_create_autocmd("FileType", {
   group = augroup("close_with_q"),
   pattern = {
+    "fugitive",
     "help",
     "qf",
     "spectre_panel",
