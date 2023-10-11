5867b74f79ad9fa9bbc95ac9063afae6be6bd238
Steve McKinney <steve.mckinney@gmail.com>
2023-10-10 23:30:06 -0400

    [nvim] Add JavaScript jest testing

--- config/nvim/lua/steve/plugins/ide_neotest.lua
+++ config/nvim/lua/steve/plugins/ide_neotest.lua
@@ -2,6 +2,7 @@ return {
   {
     "nvim-neotest/neotest",
     dependencies = {
+      "nvim-neotest/neotest-jest",
       "nvim-neotest/neotest-vim-test",
       "vim-test/vim-test",
     },
@@ -26,6 +27,14 @@ return {
         adapters = {
           require("neotest-vim-test")({
             allow_file_types = { "lua" },
+          }),
+          require("neotest-jest")({
+            jestCommand = "npm test --",
+            jestConfigFile = "jest.config.js",
+            env = { CI = true },
+            cwd = function()
+              return vim.fn.getcwd()
+            end,
           }),
         },
       })
