186c271510250d095760b5d5340cfa2e4f89092f
Steve McKinney <steve.mckinney@gmail.com>
2023-10-22 14:25:47 -0400

    [nvim] Add more comments

--- config/nvim/lua/steve/plugins/powers.lua
+++ config/nvim/lua/steve/plugins/powers.lua
@@ -51,6 +51,7 @@ return {
     },
   },
 
+  -- An all in one plugin for converting text case in Neovim. It converts a piece of text to an indicated string case and also is capable of bulk replacing texts without changing cases
   {
     "johmsalas/text-case.nvim",
     event = "VeryLazy",
