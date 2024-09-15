068248cbaa0743e2c1fa58a55123e39a0d41518a
Steve McKinney <steve.mckinney@gmail.com>
2024-09-15 16:14:00 -0400

    [nvim] Update lazy.nvim config according to updated README

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -6,12 +6,13 @@ vim.g.mapleader = ","
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- Bootstrap lazy.nvim, for managing and loading plugins lazily.
+-- Bootstrap lazy.nvim
+-- Lazy.nvim manages and loads plugins lazily.
 -- The setup checks for the existence of lazy.nvim. If it's not found, it clones the
 -- latest stable version of lazy.nvim from GitHub.
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local out = vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
@@ -19,18 +20,24 @@ if not vim.loop.fs_stat(lazypath) then
     "--branch=stable", -- latest stable release
     lazypath,
   })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
+
+-- Setup lazy.nvim
 require("lazy").setup({
   spec = {
     { import = "steve.plugins" },
     { import = "steve.plugins.editor" },
     { import = "steve.plugins.ide" },
-  },
-  -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-  -- have outdated releases, which may break your Neovim install.
-  defaults = {
-    version = false,
   },
   -- disable config change notifications
   change_detection = {
