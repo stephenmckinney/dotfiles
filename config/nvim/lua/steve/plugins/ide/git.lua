78d2209498e6affa4bb20580400cf47abd21f7e1
Steve McKinney <steve.mckinney@gmail.com>
2024-10-07 12:22:13 -0400

    [nvim] Fix git blame and open file in GitHub

--- config/nvim/lua/steve/plugins/ide/git.lua
+++ config/nvim/lua/steve/plugins/ide/git.lua
@@ -6,14 +6,17 @@ return {
   {
     "tpope/vim-fugitive",
     cmd = "Git",
+    dependencies = { "tpope/vim-rhubarb" },
     -- stylua: ignore
     keys = {
       -- git
-      { "<leader>gB", "<cmd>Git blame<cr>",   desc = "`git blame` (Fugitive)" },
-      { "<leader>gc", "<cmd>Git commit<cr>",  desc = "`git commit`" },
-      { "<leader>gd", "<cmd>Gvdiffsplit<cr>", desc = "`git diff`" },
-      { "<leader>gr", "<cmd>Gread<cr>",       desc = "Gread | `git checkout` (restore working tree file)" },
-      { "<leader>gw", "<cmd>Gwrite<cr>",      desc = "Gwrite | `git add` (stage file)" },
+      { "<leader>gb", "<cmd>Gitsigns toggle_current_line_blame<cr>", desc = "`git blame` (inline)" },
+      { "<leader>gB", "<cmd>Git blame<cr>",                          desc = "`git blame` (Fugitive)" },
+      { "<leader>gc", "<cmd>Git commit<cr>",                         desc = "`git commit`" },
+      { "<leader>gd", "<cmd>Gvdiffsplit<cr>",                        desc = "`git diff`" },
+      { "<leader>gh", "<cmd>GBrowse<cr>",                            desc = "Open file in GitHub" },
+      { "<leader>gr", "<cmd>Gread<cr>",                              desc = "Gread | `git checkout` (restore working tree file)" },
+      { "<leader>gw", "<cmd>Gwrite<cr>",                             desc = "Gwrite | `git add` (stage file)" },
       {
         "<leader>gs",
         function()
@@ -26,21 +29,6 @@ return {
       -- diff
       { "<leader>dg", ":diffget<bar>diffudpate<cr>", mode = "x", desc = "diffget" },
       { "<leader>dp", ":diffput<bar>diffupdate<cr>", mode = "x", desc = "diffput" },
-    },
-  },
-
-  -- Inline git blame
-  {
-    "f-person/git-blame.nvim",
-    event = "VeryLazy",
-    name = "gitblame",
-    opts = {
-      enabled = false,
-    },
-    -- stylua: ignore
-    keys = {
-      { "<leader>gb", "<cmd>GitBlameToggle<cr>",        desc = "`git blame` (inline)" },
-      { "<leader>gh", "<cmd>GitBlameOpenCommitURL<cr>", desc = "Open GitHub commit URL" },
     },
   },
 }
