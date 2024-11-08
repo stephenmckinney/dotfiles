633bb59ea718c88fb0236e368ffd08717253b07c
Steve McKinney <steve.mckinney@gmail.com>
2024-11-08 09:21:39 -0500

    Set default format options and set rubocop as the ruby formatter

--- config/nvim/lua/steve/plugins/ide/formatting.lua
+++ config/nvim/lua/steve/plugins/ide/formatting.lua
@@ -5,8 +5,22 @@ return {
     event = { "BufWritePre" },
     cmd = { "ConformInfo" },
     opts = {
+      -- Set default values when calling conform.format()
+      -- Will also affect the default values for format_on_save/format_after_save
+      default_format_opts = {
+        lsp_format = "fallback",
+      },
+      format_on_save = {
+        timeout_ms = 500,
+      },
+      -- Set the log level. Use `:ConformInfo` to see the location of the log file.
+      log_level = vim.log.levels.ERROR,
+      -- Conform will notify you when a formatter errors
+      notify_on_error = true,
+      -- Formatters
       formatters_by_ft = {
         ["css"] = { "prettier" },
+        ["fish"] = { "fish_indent" },
         ["graphql"] = { "prettier" },
         ["handlebars"] = { "prettier" },
         ["html"] = { "prettier" },
@@ -18,15 +32,19 @@ return {
         ["lua"] = { "stylua" },
         ["markdown"] = { "prettier" },
         ["markdown.mdx"] = { "prettier" },
+        ["ruby"] = { "rubocop" },
         ["scss"] = { "prettier" },
+        ["sh"] = { "shfmt" },
         ["typescript"] = { "prettier" },
         ["typescriptreact"] = { "prettier" },
         ["vue"] = { "prettier" },
         ["yaml"] = { "prettier" },
       },
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
+      -- The options you set here will be merged with the builtin formatters.
+      formatters = {
+        rubocop = {
+          command = { "bundle exec rubocop" },
+        },
       },
     },
     keys = {
@@ -34,10 +52,7 @@ return {
       {
         "<leader>cf",
         function()
-          require("conform").format({
-            timeout_ms = 500,
-            lsp_fallback = true,
-          })
+          require("conform").format()
         end,
         desc = "Format",
         mode = { "n", "v" },
