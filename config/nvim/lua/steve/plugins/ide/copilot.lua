eb5945e27b96f4dd6ad89b2c4dd2d5f11522d8eb
Steve McKinney <steve.mckinney@gmail.com>
2025-03-24 23:28:42 -0400

    [nvim] Update copilot

--- config/nvim/lua/steve/plugins/ide/copilot.lua
+++ config/nvim/lua/steve/plugins/ide/copilot.lua
@@ -4,15 +4,26 @@ return {
     cmd = "Copilot",
     event = "InsertEnter", -- Auto completion is triggered when insert mode is entered.
     config = function()
-      vim.g.copilot_no_maps = true
       vim.g.copilot_no_tab_map = true
+      vim.g.copilot_no_maps = true
 
-      vim.api.nvim_set_keymap("i", "<C-k>", [[copilot#Accept("\<CR>")]], { silent = true, expr = true, script = true })
-      vim.api.nvim_set_keymap("i", "<C-j>", [[copilot#Dismiss()]], { silent = true, expr = true, script = true })
-      vim.api.nvim_set_keymap("i", "<C-u>", [[copilot#Previous()]], { silent = true, expr = true, script = true })
-      vim.api.nvim_set_keymap("i", "<C-d>", [[copilot#Next()]], { silent = true, expr = true, script = true })
-      -- TODO: do we really need this if copilot is always active?
-      -- vim.api.nvim_set_keymap("i", "<C-Space>", [[copilot#Suggest()]], { silent = true, expr = true, script = true })
+      -- Suggest
+      -- vim.keymap.set("i", "<C-h>", "<Plug>(copilot-suggest)") -- Trigger suggestion - "help me suggest"
+
+      -- Suggestion navigation
+      vim.keymap.set("i", "<M-]>", "<Plug>(copilot-next)") -- Next suggestion
+      vim.keymap.set("i", "<M-[>", "<Plug>(copilot-previous)") -- Previous suggestion
+
+      -- Accepting suggestions
+      vim.keymap.set("i", "<C-k>", 'copilot#Accept("\\<CR>")', { -- Accept whole suggestion - most common action gets another home row key
+        expr = true,
+        replace_keycodes = false,
+      })
+      vim.keymap.set("i", "<C-f>", "<Plug>(copilot-accept-word)") -- Accept word
+      vim.keymap.set("i", "<C-l>", "<Plug>(copilot-accept-line)") -- Accept line
+
+      -- Dismissing
+      vim.keymap.set("i", "<C-c>", "<Cmd>call copilot#Dismiss()<CR>") -- Dismiss - "cancel"
     end,
     keys = {
       { "<leader>ip", "<cmd>Copilot status<cr>", desc = "Co[p]ilot Status" },
