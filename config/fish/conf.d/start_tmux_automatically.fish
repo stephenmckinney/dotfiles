36a7c5d1a9b6ff1a189515b023ee25fc6aa8187b
Steve McKinney <steve.mckinney@gmail.com>
2025-05-20 13:12:00 -0400

    [fish] Fix auto start tmux

--- config/fish/conf.d/start_tmux_automatically.fish
+++ config/fish/conf.d/start_tmux_automatically.fish
@@ -2,11 +2,7 @@ function start_tmux_automatically --on-event fish_prompt
   if status is-interactive
     # Check if we're not already in a tmux session
     if not set -q TMUX
-      # Don't start tmux if we're in VS Code
-      if not set -q TERM_PROGRAM
-        # Start your tmux session function
-        start_tmux_session_with_layout
-      end
+      start_tmux_session_with_layout
     end
   end
 end
