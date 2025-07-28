e6d7e4a68cec914bc281378a7a900b644bf60151
Steve McKinney <steve.mckinney@gmail.com>
2025-07-28 18:15:39 -0400

    Remove deprecated git scripts for branch, tag, log, and history management (#14)

--- bin/nvim-telescope-git-bcommits.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# $1 is the file path from Telescope
-# 2aecdee Steve McKinney 3 weeks ago Lorem ipsum dolor sit amet
-# git log --pretty="format:%h %cn %cr %s" --follow $1 >/tmp/gitlog && cat /tmp/gitlog
-git log --pretty=oneline --follow $1 >/tmp/gitlog && cat /tmp/gitlog
